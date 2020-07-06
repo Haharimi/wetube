d6f4cd8804101e30e9bfabd3d5ee85f3c85aa020
haharimi <haharimi@naver.com>
2020-07-06 22:19:34 +0900

    Local Authenticaion with part one

--- passport.js
+++ passport.js
@@ -2,3 +2,6 @@ import passport from "passport";
 import User from "./models/User;";
 
 passport.user(User.createStrategy());
+
+passport.serializeUser(User.serializeUser()); // 쿠키에는 오직 user.id만 담아보내라는 의미
+passport.deserializeUser(User.deserializeUser());
