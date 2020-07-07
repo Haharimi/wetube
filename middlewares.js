db82951b364f4941765b2b4cb2178fd08e95ccbd
haharimi <haharimi@naver.com>
2020-07-07 14:48:45 +0900

    onlyPublic and onlyPrivate Middleware

--- middlewares.js
+++ middlewares.js
@@ -10,4 +10,24 @@ export const localsMiddleware = (req, res, next) => {
   next();
 };
 
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    // req.user가 존재한다면, 즉 login 상태라면 home으로 redirect
+    res.redirect(routes.home);
+  } else {
+    // req.user가 존재하지않는다면, 즉 logout 상태라면 다음으로 진행
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    // req.user가 존재한다면, 즉 login 상태라면 다음으로 진행
+    next();
+  } else {
+    // req.user가 존재하지않는다면, 즉 logout 상태라면  home으로 redirect
+    req.redirect(routes.home);
+  }
+};
+
 export const uploadVideo = multerVideo.single("videoFile");
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,17 @@
 import express from "express";
 import routes from "../routes";
-import { users, userDetail, editProfile, changePassword } from "../controller/userController";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword,
+} from "../controller/userController";
+import { onlyPublic } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPublic, editProfile);
+userRouter.get(routes.changePassword, onlyPublic, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
