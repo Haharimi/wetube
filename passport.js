1e20d2d055b63a4db871ba572fe9a8f8d1f0874e
haharimi <haharimi@naver.com>
2020-07-07 21:49:59 +0900

    Github Lon In completed

--- passport.js
+++ passport.js
@@ -2,21 +2,20 @@ import passport from "passport";
 import GithubStrategy from "passport-github";
 import User from "./models/User";
 import { githubLoginCallback } from "./controller/userController";
+import routes from "./routes";
 
 passport.use(User.createStrategy());
 
-passport.use(new GithubStrategy({
-    clientID: GH_ID,
-    clientSecret: GH_SECRET,
-    callbackURL: `http://localhost:${PORT}/auth/github/callback`
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      // USER가 Github를 갔다가 정보를 가지고 돌아왔을 때 실행되는 함수 
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });,
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+    },
     githubLoginCallback
-  }
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
