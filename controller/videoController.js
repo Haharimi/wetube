627fe26d09a9930c2c5794d01fc224ebcef98644
haharimi <haharimi@naver.com>
2020-07-12 21:28:02 +0900

    #10.0 Making  ApiRoter, constroller

--- controller/videoController.js
+++ controller/videoController.js
@@ -97,3 +97,21 @@ export const deleteVideo = async (req, res) => {
   }
   res.redirect(routes.home);
 };
+
+// REgister Video View
+
+export const postRegisterView = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    video.views += 1; // video.views = video.views + 1;
+    video.save();
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
